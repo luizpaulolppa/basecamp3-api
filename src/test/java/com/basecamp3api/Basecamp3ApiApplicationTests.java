5f23b85ba6719eeec0ee31c94dbf4e35beaaabd0
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2020-09-13 16:53:15 -0300

    Criando projeto inicial

---
+++ src/test/java/com/basecamp3api/Basecamp3ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basecamp3api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Basecamp3ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
